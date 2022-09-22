0fcb797ea49ab228d8eb052ee7f3192fe37f5740
Igor Gotovchik <gotovchik.igor@yandex.ru>
2022-09-22 20:33:49 +0300

    №3 task solved

---
+++ exercises/day_of_the_week.py
@@ -0,0 +1,11 @@
+# 3. По заданному номеру дня недели вывести его название
+
+def day_of_the_week(n):
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    print(days[n-1])
+
+day = int(input('Введите номер дня недели:'))
+day_of_the_week(day)
+
+day = int(input('Введите номер дня недели:'))
+day_of_the_week(day)
---
+++ exercises/two_nums.py
@@ -0,0 +1,9 @@
+# 2. Даны два числа. Показать большее и меньшее число
+
+first_number = 78
+second_number = 35
+
+if first_number > second_number:
+    print(f'Большее {first_number}, меньшее {second_number}')
+else:
+    print(f'Большее {second_number}, меньшее {first_number}')
