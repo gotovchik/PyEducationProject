6f63bc320eef8aae51135fdebe14023ae669e225
Igor Gotovchik <gotovchik.igor@yandex.ru>
2022-09-25 20:09:14 +0300

    add is_prime.py

---
+++ exercises/is_prime.py
@@ -0,0 +1,10 @@
+def is_prime(num):
+    prime = num > 1 and (num % 2 != 0 or num == 2) and (num % 3 != 0 or num == 3)
+    i = 5
+    d = 2
+
+    while prime and i * i <= num:
+        prime = num % i != 0
+        i += d
+        d = 6 - d
+    return prime
