077e7e78b29b5add6506f82e18f5980a6bf7fe0b
Igor Gotovchik <gotovchik.igor@yandex.ru>
2022-10-17 12:16:47 +0300

    task_03.py done

---
+++ homework_07/task_03.py
@@ -0,0 +1,42 @@
+"""
+Задание 3.
+Реализовать базовый класс Worker (работник),
+в котором определить публичные атрибуты name, surname, position (должность),
+и защищенный атрибут income (доход). Последний атрибут должен ссылаться
+на словарь, содержащий элементы: оклад и премия, например,
+{"wage": wage, "bonus": bonus}.
+Создать класс Position (должность) на базе класса Worker.
+В классе Position реализовать публичные методы
+получения полного имени сотрудника (get_full_name) и дохода с учетом премии
+(get_total_income).
+Проверить работу примера на реальных данных
+(создать экземпляры класса Position, передать данные,
+проверить значения атрибутов, вызвать методы экземпляров).
+П.С. попытайтесь добить вывода информации о сотруднике
+также через перегрузку __str__
+__str__(self) - вызывается функциями str, print и format.
+Возвращает строковое представление объекта.
+"""
+
+
+class Worker:
+    name = 'Igor'
+    surname = 'Gotovchik'
+    position = 'Universal employer store'
+    _income = {'wage': 35000, 'bonus': 28000}
+
+
+class Position(Worker):
+
+    def get_full_name(self):
+        return f'{self.surname} {self.name}'
+
+    def get_total_income(self):
+        return self._income['wage'] + self._income['bonus']
+
+    def __str__(self):
+        return f'{self.get_full_name()}\n{self.get_total_income()}'
+
+
+employer = Position()
+print(employer)
