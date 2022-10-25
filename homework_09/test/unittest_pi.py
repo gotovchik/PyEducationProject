5606ced38ade384e47a30b85e7c2dbe653c64a98
Igor Gotovchik <gotovchik.igor@yandex.ru>
2022-10-25 12:09:52 +0300

    update unittest_pi.py

--- homework_09/test/unittest_pi.py
+++ homework_09/test/unittest_pi.py
@@ -3,8 +3,17 @@ from homework_09.code.pi import truncate
 
 
 class TestTruncate(unittest.TestCase):
-    def test_truncate(self):
+    def test_truncate0(self):
         self.assertEqual(truncate(5), 3.14159)
+
+    def test_truncate1(self):
+        self.assertEqual(truncate(4), 3.1415)
+
+    def test_truncate2(self):
+        self.assertEqual(truncate(3), 3.141)
+
+    def test_truncate3(self):
+        self.assertEqual(truncate(2), 3.14)
 
 
 if __name__ == '__main__':
