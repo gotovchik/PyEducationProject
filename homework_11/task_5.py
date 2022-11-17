06cbf9f8001a52765ae3990b91f950c5bea3e8c4
Igor Gotovchik <gotovchik.igor@yandex.ru>
2022-11-17 11:06:07 +0300

    def task_5.py

--- homework_11/task_5.py
+++ homework_11/task_5.py
@@ -7,3 +7,20 @@
 Подсказки:
 --- используйте модуль chardet, иначе задание не засчитается!!!
 """
+import subprocess
+import chardet
+
+args1 = ['ping', 'yandex.ru']
+args2 = ['ping', 'youtube.com']
+
+YA_PING = subprocess.Popen(args1, stdout=subprocess.PIPE)
+for line in YA_PING.stdout:
+    result = chardet.detect(line)
+    line = line.decode(result['encoding']).encode('utf-8')
+    print(line.decode('utf-8'))
+
+YT_PING = subprocess.Popen(args2, stdout=subprocess.PIPE)
+for line in YT_PING.stdout:
+    result = chardet.detect(line)
+    line = line.decode(result['encoding']).encode('utf-8')
+    print(line.decode('utf-8'))
